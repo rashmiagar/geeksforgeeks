c239f3c6433e8ee8472159d363ee9a60fbd31c1f
rashmi1985 <rashmi@1985.co.in>
2017-10-06 16:04:03 +0530

    adding klargest

--- KLargest.java
+++ KLargest.java
@@ -12,3 +12,35 @@
 
 
 
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class KLargest {
+	public static void main (String[] args) {
+		Scanner scn = new Scanner(System.in);
+		int numInputs = Integer.parseInt(scn.nextLine());
+
+		while(numInputs > 0){
+			String b = scn.nextLine();
+			String[] inputs = b.split(" ");  // N k
+			ArrayList<Integer> result = new ArrayList<Integer>(Collections.nCopies(Integer.parseInt(inputs[1]), 0));
+			String[] arr = scn.nextLine().split(" ");
+
+			result.add(Integer.parseInt(arr[0]));
+			for(int i=1; i < arr.length; i++){
+				for(int j=0; j < result.size(); j++){
+					if(Integer.parseInt(arr[i]) > result.get(j)){
+						result.add(j, Integer.parseInt(arr[i]));
+						break;
+					}
+				}
+			}
+			for(int i=0; i < Integer.parseInt(inputs[1]); i++){
+				System.out.print(result.get(i) + " ");
+			}
+			System.out.println();
+			numInputs--;
+		}
+	}
+}
